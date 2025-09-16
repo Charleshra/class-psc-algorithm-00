8c05fa35a98f6ab1153c797e8679f2ce6f298d47
Charleshra <112990000+Charleshra@users.noreply.github.com>
2025-09-16 19:59:27 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		String salaDeseJava = "219";
+		String salaInformada "";
+		Scanner Scanner = new Scanner (System.in);
+		System.out.System.out.print("Digite o numero");
+		salaInformada = Scanner.new
+		
+		if (salaDeseJava) == salaInformada
+		System.out.print(x,"voce encontrou sua em sala")
+	}
+}
